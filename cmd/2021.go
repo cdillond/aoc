37826a798b44a9da8d41909b843a0de5f66ddcaa
cdillond <93200981+cdillond@users.noreply.github.com>
2024-11-22 01:35:26 -0500

    updated problem years

---
+++ cmd/2021.go
@@ -0,0 +1,13 @@
+//go:build 2021
+
+package main
+
+import (
+	_ "aoc/2021/d1"
+	_ "aoc/2021/d2"
+)
+
+func init() {
+	base = "../2021/"
+	year = 2021
+}
--- cmd/2024.go
+++ cmd/2024.go
@@ -2,11 +2,6 @@
 
 package main
 
-import (
-	_ "aoc/2024/d1"
-	_ "aoc/2024/d2"
-)
-
 func init() {
 	base = "../2024/"
 	year = 2024
