9ced5bea056b4bb0c3e7a51dafce83b54c8e7e96
cdillond <93200981+cdillond@users.noreply.github.com>
2024-12-06 16:49:18 -0500

    day 6 solutions

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/cdillond/aoc/cmd/html"
 
 	// Update this import path when solving a new problem
-	puzzle "github.com/cdillond/aoc/2024/d4"
+	puzzle "github.com/cdillond/aoc/2024/d6"
 )
 
 func main() {
